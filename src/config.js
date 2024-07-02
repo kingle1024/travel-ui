b7142d83edaa736fdfd856a6138438b82a46c3f3
kingle1024 <teran1024@naver.com>
2024-07-02 23:11:32 +0900

    🚨 Fix: 운영 배포 build 시 production 명시

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.MODE_ENV === 'production'
+const API_URL = process.env.NODE_ENV === 'production'
   ? process.env.REACT_APP_API_URL_PRODUCTION
   : process.env.REACT_APP_API_URL_LOCAL;
 
