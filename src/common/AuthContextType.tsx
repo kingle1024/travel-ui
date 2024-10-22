82892061b6abbd8d0651bb58da70598f946e66a3
kingle1024 <teran1024@naver.com>
2024-10-22 23:03:51 +0900

    ✨ Feat: 로그아웃 시 메인 페이지로 이동, 마이페이지 구현

--- src/common/AuthContextType.tsx
+++ src/common/AuthContextType.tsx
@@ -1,26 +1,35 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+interface User {
+  name: string; // 예시로 사용자 이름을 포함
+}
 
 interface AuthContextType {
   isLoggedIn: boolean;
-  login: (token: string) => void;
+  user: User | null;
+  login: (token: string, userInfo: User) => void;
   logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(!!localStorage.getItem('jwtToken'));
+  const [user, setUser] = useState<User | null>(null);
 
   // 로그인 시 JWT 토큰을 로컬 스토리지에 저장하고 로그인 상태 업데이트
-  const login = (token: string) => {
+  const login = (token: string, userInfo: User) => {
     localStorage.setItem('jwtToken', token);
     setIsLoggedIn(true);
+    setUser(userInfo);
   }
   
   // 로그아웃 시 로컬 스토리지에서 JWT 토큰을 삭제하고 로그인 상태 업데이트
   const logout = () => {
     localStorage.removeItem('jwtToken');
     setIsLoggedIn(false);
+    setUser(null);
+    window.location.href = '/'; // 메인 페이지로 이동
   }
 
   // 컴포넌트가 처음 마운트될 때 JWT 토큰의 존재 여부를 확인하여 상태를 업데이트
@@ -30,7 +39,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
