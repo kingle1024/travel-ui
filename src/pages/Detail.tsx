2906d201fa8b67e28c8bc926f783614496ccb318
kingle1024 <teran1024@naver.com>
2024-11-21 22:37:21 +0900

    🔨 Refactor: 변수 const 변경

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -62,7 +62,7 @@ const Detail: React.FC = () => {
 
   const handleLikeClick = async () => {
     try {
-      let token = localStorage.getItem('jwtToken');
+      const token = localStorage.getItem('jwtToken');
       if(!token) {
         alert('로그인 후에 좋아요를 누를 수 있습니다.');
         return;
