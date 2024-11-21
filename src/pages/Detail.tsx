f6153b634f0b18a1a196fe010e7b6e4f6dabc4e9
kingle1024 <teran1024@naver.com>
2024-11-21 22:36:39 +0900

    ✨ Feat: 좋아요 버튼 구현

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -9,26 +9,41 @@ import API_URL from "../config";
 import CommentForm from "../components/CommentForm";
 import './Detail.css';
 import { Helmet } from "react-helmet";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { useAuth } from "../common/AuthContextType";
 
 const Detail: React.FC = () => {
+  const { user, logout } = useAuth();
   const { id } = useParams<{ id: string }>();
   const history = useHistory();
   const [product, setProduct] = useState<ProductDetail | null>(null);
   const [regions, setRegions] = useState<RegionDetail[]>([]);
   const [comments, setComments] = useState<CommentDetail[]>([]);
-  
+  const [like, setLike] = useState<boolean>(false);
+
   useEffect(() => {
     const fetchProductDetail = async () => {
       try {
+        const token = localStorage.getItem('jwtToken');
+        const rqConfig:any = {
+          headers: {}
+        };
+        if(token) {
+          rqConfig.headers['Authorization'] = `Bearer ${token}`;
+        }
+
         const response = await axios.get<{ 
           product: ProductDetail; 
           regions: RegionDetail[]; 
-          comments: CommentDetail[]; 
-        }>(`${API_URL}/detail/${id}`);
+          comments: CommentDetail[];
+          like: boolean;
+        }>(`${API_URL}/detail/${id}`, rqConfig);
 
         setProduct(response.data.product);
         setRegions(response.data.regions);
         setComments(response.data.comments);
+        setLike(response.data.like);
       } catch (error) {
         console.error('Error fetching product detail:', error);
       }
@@ -43,6 +58,46 @@ const Detail: React.FC = () => {
   
   const handleTitleClick = () => {
     history.push('/');
+  }
+
+  const handleLikeClick = async () => {
+    try {
+      let token = localStorage.getItem('jwtToken');
+      if(!token) {
+        alert('로그인 후에 좋아요를 누를 수 있습니다.');
+        return;
+      }
+
+      const rqConfig: any = {
+        headers: {}
+      };
+      if (token) {
+        rqConfig.headers['Authorization'] = `Bearer ${token}`;
+      }
+  
+      const url = `${API_URL}/likes/${id}`;
+      if (like) {
+        // 좋아요 취소
+        await axios.delete(url, rqConfig);
+      } else {
+        // 좋아요
+        await axios.post(url, {}, rqConfig);
+      }
+  
+      // 좋아요 상태 토글
+      setLike((prevLiked) => !prevLiked);
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        if (error.response.status === 401) {
+            alert('로그인 정보가 없어 메인 페이지로 이동합니다.');
+            logout();
+        } else {
+            console.error('Error fetching product detail:', error);
+        }
+    } else {
+        console.error('Unexpected error:', error);
+    }
+    }
   }
 
   return (
@@ -75,8 +130,25 @@ const Detail: React.FC = () => {
                   방문하기
                 </IonButton>                
               </div>
-
+              
               <div className='detail-content'> 
+                <div className='post-content'>
+                  <button 
+                    onClick={handleLikeClick} 
+                    style={{
+                      border: 'none',
+                      cursor: 'pointer',
+                      padding: '10px',
+                      borderRadius: '5px',
+                    }}
+                  >
+                    <FontAwesomeIcon 
+                      icon={faThumbsUp} 
+                      style={{ color: like ? 'blue' : 'black' }} // 상태에 따라 아이콘 색상 변경
+                    />
+                    좋아요
+                  </button>
+                </div>
                 <h3>이동순서</h3>
                 {regions.map((region, index) => (
                   <div key={region.regionCd}>
